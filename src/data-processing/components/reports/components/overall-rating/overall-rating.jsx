1659718438abd1fadd872de74e5ef49a02d20bba
Dan Richman <wendi.dani@gmail.com>
2016-08-29 14:03:31 -0400

    Charts updated to use Recharts library

--- src/data-processing/components/reports/components/overall-rating/overall-rating.jsx
+++ src/data-processing/components/reports/components/overall-rating/overall-rating.jsx
@@ -1,49 +1,116 @@
 import React from 'react';
-import NVD3Chart from 'react-nvd3';
-import BarChart from 'react-chartjs';
-
-import styles from './overall-rating.css';
+// import NVD3Chart from 'react-nvd3';
+// import BarChart from 'react-chartjs';
+// import styles from './overall-rating.css';
+import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, PieChart, Pie} from 'Recharts';
 
 export default class OverallRating extends React.Component {
     render() {
         var average = 4; //This needs to be pulled from store, less than or equal to 5
         var leftover = 5 - average; //this is the rest of the pie chart, 5-average
-        console.log(leftover);
-        var averagePieData = [
-            {
-                'label': 'Average:',
-                'value': average
-            },
-            {
-                'label' : '',
-                'value': leftover
-            }
-        ];
-        console.log(averagePieData);
-        var averageBarData = {
-            labels: ["Q1", "Q2", "Q3", "Q4", "Q5"],
-            datasets: [
-                {
-                    label: "Bar Chart Example",
-                    data: [1,2,3,4,2.5],
-                }
-            ]
+        // var averagePieData = [
+        //     {
+        //         'label': 'Average:',
+        //         'value': average
+        //     },
+        //     {
+        //         'label' : '',
+        //         'value': leftover
+        //     }
+        // ];
+        // console.log(averagePieData);
+        // var averageBarData = {
+        //     labels: ['Q1', 'Q2', 'Q3', 'Q4', 'Q5'],
+        //     datasets: [
+        //         {
+        //             data: [1,2,3,4,2.5],
+        //         }
+        //     ]
 
-        };
+        // };
+        // console.log(averageBarData);
+        const barChartData = [
+            { name: '1', score: 1 },
+            { name: '2', score: 2 },
+            { name: '3', score: 3 },
+            { name: '4', score: 4 },
+            { name: '5', score: 5 },
+            { name: '6', score: 2.5 },
+            { name: '7', score: 4.5 },
+            { name: '8', score: 1 },
+            { name: '9', score: 2 },
+            { name: '10', score: 3 },
+            { name: '11', score: 4 },
+            { name: '12', score: 5 },
+            { name: '13', score: 2.5 },
+            { name: '14', score: 4.5 },
+            { name: '15', score: 1 },
+            { name: '16', score: 2 },
+            { name: '17', score: 3 },
+            { name: '18', score: 4 },
+            { name: '19', score: 5 },
+            { name: '20', score: 2.5 },
+            { name: '21', score: 4.5 },
+            { name: '22', score: 1 },
+            { name: '23', score: 2 },
+            { name: '24', score: 3 },
+            { name: '25', score: 4 },
+            { name: '26', score: 5 },
+            { name: '27', score: 2.5 },
+            { name: '28', score: 4.5 },
+            { name: '29', score: 1 },
+            { name: '30', score: 2 },
+            { name: '31', score: 3 },
+            { name: '32', score: 4 },
+            { name: '33', score: 5 },
+            { name: '34', score: 2.5 },
+            { name: '35', score: 4.5 },
+            { name: '36', score: 1 },
+            { name: '37', score: 2 },
+            { name: '38', score: 3 },
+            { name: '39', score: 4 },
+            { name: '41', score: 5 },
+            { name: '41', score: 2.5 },
+            { name: '42', score: 4.5 },
+            { name: '43', score: 1 },
+            { name: '44', score: 2 },
+            { name: '45', score: 3 },
+            { name: '46', score: 4 },
+            { name: '47', score: 5 },
+            { name: '48', score: 2.5 },
+            { name: '49', score: 4.5 },
+            { name: '50', score: 1 },
+            { name: '51', score: 2 },
+            { name: '52', score: 3 },
+            { name: '53', score: 4 },
+            { name: '54', score: 5 },
+            { name: '55', score: 2.5 },
+            { name: '56', score: 4.5 },
+            { name: '57', score: 1 },
+            { name: '58', score: 2 },
+            { name: '59', score: 3 },
+            { name: '60', score: 4 },
+        ];
+
+        const pieChartData = [
+            { name: 'Score', value: average },
+            { name: '', value: leftover }
+        ];
+        // const COLORS = ['#0088FE', '#FFFFFF'];
         return (
             <div>
-                <div className={styles.averageDonut}>
-                    <h3> Donut Average Chart </h3>
-                    <div className={styles.donutChart}>
-                    <NVD3Chart id="donutChart" type="pieChart" datum={averagePieData} x="label" y="value" donut="true" showLabels="false"/>
-                    </div>
-                </div>
-                <div className={styles.averageQuestions}>
-                    <h3> Multi Bar Chart </h3>
-                    <div className={styles.barChart}>
-                    <BarChart data={averageBarData} width="400" height="400"/>
-                    </div>
-                </div>
+                <PieChart width={800} height={400}>
+                    <Pie data={pieChartData} cx={500} cy={200} innerRadius={40} outerRadius={80} fill="#82ca9d"/>
+                    <Tooltip/>
+                </PieChart>
+                <BarChart width={600} height={300} data={barChartData} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                    <XAxis dataKey="name"/>
+                    <YAxis/>
+                    <CartesianGrid strokeDasharray="3 3"/>
+                    <Tooltip/>
+                    <Legend />
+                    <Bar dataKey="score" fill="#82ca9d" />
+                </BarChart>
             </div>
         );
     }
