e70b6579866a55ebd3fb37d11b79e698d4831c2d
Dan Richman <wendi.dani@gmail.com>
2016-08-30 11:36:26 -0400

    Reports page has inline charts, correct colors

--- src/data-processing/components/reports/components/overall-rating/overall-rating.jsx
+++ src/data-processing/components/reports/components/overall-rating/overall-rating.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import styles from './overall-rating.css';
-import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, PieChart, Pie, RadialBar, RadialBarChart} from 'Recharts';
+import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, PieChart, Pie, Cell} from 'Recharts';
 
 export default class OverallRating extends React.Component {
     render() {
         var average = 4; //This needs to be pulled from store, less than or equal to 5
         var leftover = 5 - average; //this is the rest of the pie chart, 5-average
+        //bar chart data needs to be pulled from DB. Have to calculate the average of each
+        //question, then put it in an array of objects
+        //format [ {name: index, score: value} ]
         const barChartData = [
             { name: '1', score: 1 },
             { name: '2', score: 2 },
@@ -68,32 +71,29 @@ export default class OverallRating extends React.Component {
             { name: '59', score: 3 },
             { name: '60', score: 4 },
         ];
-
         const pieChartData = [
             { name: 'Score', value: average },
             { name: '', value: leftover }
         ];
-        const radialChartData = [
-            { name: 'average', value: average }
-        ];
-        // const COLORS = ['#0088FE', '#FFFFFF'];
+        const COLORS = ['#32CD32', '#D3D3D3'];
         return (
-            <div>
-                <PieChart width={400} height={400} className={styles.averageDonut}>
-                    <Pie data={pieChartData} cx={100} cy={200} innerRadius={40} outerRadius={80} fill="#82ca9d"/>
+            <div className={styles.container}>
+                <PieChart width={200} height={200} className={styles.averageDonut}>
+                    <Pie data={pieChartData} cx='50%' cy='50%' innerRadius={40} outerRadius={80} fill="#82ca9d" startAngle={90} endAngle={-270}>
+                        {
+                            pieChartData.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]}/>)
+                        }
+                    </Pie>
                     <Tooltip/>
                 </PieChart>
-                <BarChart width={600} height={300} data={barChartData} margin={{ top: 5, right: 30, left: 20, bottom: 5 }} className={styles.averageQuestions}>
+                <BarChart width={650} height={250} data={barChartData} margin={{ top: 5, right: 30, left: 20, bottom: 5 }} className={styles.averageQuestions}>
                     <XAxis dataKey="name"/>
                     <YAxis/>
                     <CartesianGrid strokeDasharray="3 3"/>
                     <Tooltip/>
                     <Legend />
-                    <Bar dataKey="score" fill="#82ca9d" />
+                    <Bar dataKey="score" fill="#0088FE" />
                 </BarChart>
-                <RadialBarChart width={500} height={300} cx={150} cy={150} innerRadius={20} outerRadius={140} barSize={30} data={radialChartData}>
-                    <RadialBar minAngle={15} label background clockWise={true} dataKey='Total Average'/>
-                </RadialBarChart>
             </div>
         );
     }
